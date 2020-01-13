72f39d9efe4bc3b75883ecd440111061833d22df
Jan Harsa <jan.harsa@haxo.cz>
2020-01-13 14:02:05 +0100

    comm2
    Signed-off-by: Jan Harsa <jan.harsa@haxo.cz>

---
+++ app/Presenters/NecoPresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+
+use Nette\Application\UI\Presenter;
+
+class NecoPresenter extends Presenter
+{
+    public function renderNevim() {
+
+    }
+}
---
+++ app/Presenters/TestovaciPresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+class TestovaciPresenter extends Presenter
+{
+    public function renderDefault(): void {
+        $this->template->anyVariable = 'any něco';
+    }
+}
